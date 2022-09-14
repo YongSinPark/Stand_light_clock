198c0c8a54810c47a51f2a975b7e69b2870405b5
YongSin_Park <yongsin0416@gmail.com>
2022-09-14 17:47:37 +0900

    hw_2

---
+++ src/app/Listener/Listener.cpp
@@ -0,0 +1,25 @@
+#include "Listener.h"
+
+Listener::Listener(Button *button, Controller *control, ClockCheck *clock)
+{
+    powerButton = button;
+    controller = control;
+    clockCheck = clock;
+}
+
+Listener::~Listener()
+{
+}
+
+void Listener::checkEvent()
+{
+    if (powerButton->getState() == RELEASE_ACTIVE)
+    {
+        controller->updateEvent("modeButton");
+    }
+
+    if (clockCheck->isUpdate())
+    {
+        controller->updateEvent("clockUpdate");
+    }
+}
